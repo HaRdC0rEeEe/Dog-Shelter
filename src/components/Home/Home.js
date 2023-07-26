47045d2fc49f2f4be75d2f710ba907aed10e8ebf
HaRdC0rEeEe <petrpospi999@seznam.cz>
2023-07-26 09:30:37 +0200

    another commit

---
+++ src/components/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./HomeStyles.css";
+import { PageContainer } from './HomeStyle';
+
+
+const Home = () => {
+    return (
+        <PageContainer>
+            <h1>Home</h1>
+
+        </PageContainer>
+    );
+}
+
+export default Home;
